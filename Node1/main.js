776be476fe86456016ac320c0ffe39c9bee387ea
Tamirlan <tuarshintamir@gmail.com>
2023-11-19 16:00:25 +0500

    try to add counter

--- Node1/main.js
+++ Node1/main.js
@@ -1,22 +1,26 @@
 const http = require('http');
+let counter = 0;
+function incrementCounter() {
+    counter++;
+}
+
 
 const server = http.createServer((req, res) => {
-    // res.writeHead(200,
-    //     { 'Content-Type': 'text/html; charset=utf-8' })
-    // res.end('<h1>Go</h1>')
 
-
-    if (req.url === '/') {
+    if (req.url === '/home') {
+        incrementCounter();
         res.writeHead(200, { 'Content-Type': 'text/html ; charset=UTF-8' });
-        res.end('<a href="about">Перейти на страницу About  </a> <a href=404> Page 404</a>');
+        res.end(`<a href="about">Перейти на страницу About</a>\n<a href=404>Page 404</a>\n<p id="counter">${counter}</p>`);
     }
     else if (req.url === '/about') {
+        incrementCounter();
         res.writeHead(200, { 'Content-Type': 'text/html ; charset=UTF-8' });
-        res.end('<a href="/">Перейти на страницу Main  </a>');
+        res.end(`<a href="/home">Перейти на страницу Main</a> \n <p id="counter">${counter}</p>`);
     }
     else {
+        incrementCounter();
         res.writeHead(200, { 'Content-Type': 'text/html ; charset=UTF-8' });
-        res.end('<h1>Page 404 </h1>')
+        res.end(`<h1>Page 404 </h1>\n <p id="counter">${counter}</p><a href="/home">Перейти на страницу Main</a> `)
     }
 })
 
